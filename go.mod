3087d4d2f6ef3597f9274eaa72801bb25c816cbb
luc <luc.roy@gmail.com>
2023-02-23 21:33:04 -0400

    added initial estuary-auth routes to echo

--- go.mod
+++ go.mod
@@ -2,7 +2,21 @@ module auth-svc
 
 go 1.18
 
-require go-micro.dev/v4 v4.9.0
+require (
+	github.com/go-micro/plugins/v4/server/http v1.2.1
+	github.com/labstack/echo/v4 v4.10.2
+	go-micro.dev/v4 v4.9.0
+)
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/time v0.3.0 // indirect
+)
 
 require (
 	github.com/Microsoft/go-winio v0.6.0 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"go-micro.dev/v4/registry"
+	"go-micro.dev/v4/server"
+	"log"
+	"net/http"
 
-	"auth-svc/handler"
-	pb "auth-svc/proto"
-
+	httpServer "github.com/go-micro/plugins/v4/server/http"
 	"go-micro.dev/v4"
-	"go-micro.dev/v4/logger"
 
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 var (
@@ -17,19 +20,56 @@ var (
 
 func main() {
 	// Create service
-	srv := micro.NewService(
-	)
-	srv.Init(
-		micro.Name(service),
-		micro.Version(version),
+	srv := httpServer.NewServer(
+		server.Name(service),
+		server.Version(version),
+		server.Address(":1313"),
 	)
 
-	// Register handler
-	if err := pb.RegisterAuthSvcHandler(srv.Server(), new(handler.AuthSvc)); err != nil {
-		logger.Fatal(err)
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
+	authService := newAuthService()
+	authService.InitRouter(e)
+
+	hd := srv.NewHandler(e)
+	if err := srv.Handle(hd); err != nil {
+		log.Fatalln(err)
 	}
-	// Run service
-	if err := srv.Run(); err != nil {
-		logger.Fatal(err)
-	}
+
+	service := micro.NewService(
+		micro.Server(srv),
+		micro.Registry(registry.NewRegistry()),
+	)
+	service.Init()
+	service.Run()
+}
+
+type authService struct{}
+
+func newAuthService() *authService {
+	return &authService{}
+}
+
+func (a *authService) InitRouter(e *echo.Echo) {
+	e.POST("/check-api-key", a.BasicUserApiCheckHandler)
+	e.POST("/check-user-api-key", a.BasicApiUserCheckHandler)
+	e.POST("/check-user-pass", a.BasicUserPassHandler)
+}
+
+func (a *authService) BasicUserApiCheckHandler(c echo.Context) error {
+	return c.NoContent(http.StatusOK)
+}
+
+func (a *authService) BasicApiUserCheckHandler(c echo.Context) error {
+	return c.NoContent(http.StatusOK)
+}
+
+func (a *authService) BasicUserPassHandler(c echo.Context) error {
+	return c.NoContent(http.StatusOK)
 }
